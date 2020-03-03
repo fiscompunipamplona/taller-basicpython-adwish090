cf73420b4484b1ce048d8de89f2e1b298e47740f
osmargils <osmagis@hotmail.com>
2020-03-03 13:57:41 -0500

    Primer programa python que ayuda a calcular altura en caida libre

---
+++ probar.py
@@ -0,0 +1,11 @@
+t=input("Asign un valor para el tiempo:")
+t=float(t)
+
+h=input("Asigne un valor para la altura:")
+h=float(h)
+
+g=9.8
+
+print("La posicion del objeto es:")
+print(h+0.5*g*t**2)
+
